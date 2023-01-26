0ae5631a49c579c67ad36733f938c5fb5c26e29e
soichiro-nishizono <soicchi@icloud.com>
2023-01-27 07:48:52 +0900

    feat: reviewd selection function

--- review/sort/review2.py
+++ review/sort/review2.py
@@ -8,9 +8,21 @@ class Sort:
 
         return nums
 
+    def selection_sort(self, nums: list[int]) -> list[int]:
+        len_nums = len(nums)
+        for i in range(len_nums):
+            min_index = i
+            for j in range(i, len_nums):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+
+        return nums
+
 
 if __name__ == "__main__":
     import random
     nums = [random.randint(1, 1000) for _ in range(10)]
     sort = Sort()
     print(sort.bubble_sort(nums))
+    print(sort.selection_sort(nums))
