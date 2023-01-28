c0f7d364fdf0fa3a6d176e2ce582f4ee75c8356c
soichiro-nishizono <soicchi@icloud.com>
2023-01-28 16:43:50 +0900

    feat: reviewed quick sort function

--- review/sort/review3.py
+++ review/sort/review3.py
@@ -31,6 +31,29 @@ class Sort:
 
         return nums
 
+    def quick_sort(self, nums: list[int]) -> list[int]:
+        self._quick_sort(nums, 0, len(nums)-1)
+
+        return nums
+
+    def _quick_sort(self, nums: list[int], low: int, high: int) -> None:
+        if low < high:
+            partition_index = self._partition(nums, low, high)
+            self._quick_sort(nums, low, partition_index-1)
+            self._quick_sort(nums, partition_index+1, high)
+
+    def _partition(self, nums: list[int], low: int, high: int) -> int:
+        i = low - 1
+        pivot = nums[high]
+        for j in range(low, high):
+            if nums[j] < pivot:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+
+        nums[i+1] = pivot
+
+        return i+1
+
 
 if __name__ == "__main__":
     import random
@@ -39,3 +62,4 @@ if __name__ == "__main__":
     print(sort.bubble_sort(nums))
     print(sort.selection_sort(nums))
     print(sort.insertion_sort(nums))
+    print(sort.quick_sort(nums))
