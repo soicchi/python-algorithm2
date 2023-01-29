f38096fe7310982270cfc632dea991bbb0d61992
soichiro-nishizono <soicchi@icloud.com>
2023-01-29 11:08:07 +0900

    feat: reviewed insertion sort function

--- review/sort/review4.py
+++ review/sort/review4.py
@@ -19,9 +19,22 @@ class Sort:
 
         return nums
 
+    def insertion_sort(self, nums: list[int]) -> list[int]:
+        len_nums = len(nums)
+        for i in range(1, len_nums):
+            temp = nums[i]
+            j = i - 1
+            while j >= 0 and nums[j] > temp:
+                nums[j+1] = nums[j]
+                j -= 1
+            nums[j+1] = temp
+
+        return nums
+
 if __name__ == "__main__":
     import random
     nums = [random.randint(0, 1000) for _ in range(10)]
     sort = Sort()
     print(sort.bubble_sort(nums))
     print(sort.selection_sort(nums))
+    print(sort.insertion_sort(nums))
