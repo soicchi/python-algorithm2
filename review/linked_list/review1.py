d5f27fc02d391619b5efa4b42dec77d45099ed96
soichiro-nishizono <soicchi@icloud.com>
2023-01-29 14:50:40 +0900

    feat: reviewed append function in linked list

---
+++ review/linked_list/review1.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data: any, next_node: any = None) -> None:
+        self.data = data
+        self.next = next_node
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def append(self, data: any) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = new_node
+
+    def print(self) -> None:
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+
+if __name__ == "__main__":
+    l = LinkedList()
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.print()
