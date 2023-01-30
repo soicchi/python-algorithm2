dc27e33cc0aac051a856c43f7b642f04babdd12d
soichiro-nishizono <soicchi@icloud.com>
2023-01-30 09:23:09 +0900

    feat: reviewed merge sort function

--- review/sort/review5.py
+++ review/sort/review5.py
@@ -38,16 +38,55 @@ class Sort:
                 _quick_sort(nums, low, partition_index-1)
                 _quick_sort(nums, partition_index+1, high)
 
+        _quick_sort(nums, 0, len(nums)-1)
+
+        return nums
+
     def _partition(self, nums: list[int], low: int, high: int) -> int:
-        i = low - i
+        i = low - 1
         pivot = nums[high]
         for j in range(low, high):
-            if nums[j] < nums[pivot]:
+            if nums[j] < pivot:
                 i += 1
                 nums[i], nums[j] = nums[j], nums[i]
         nums[i+1] = pivot
 
         return i+1
+
+    def merge_sort(self, nums: list[int]) -> list[int]:
+        len_nums = len(nums)
+        if len_nums <= 1:
+            return nums
+        center = len_nums // 2
+        left = nums[:center]
+        right = nums[center:]
+
+        self.merge_sort(left)
+        self.merge_sort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                nums[k] = left[i]
+                i += 1
+                k += 1
+                continue
+
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+        return nums
 
 
 if __name__ == "__main__":
@@ -58,3 +97,4 @@ if __name__ == "__main__":
     print(sort.selection_sort(nums))
     print(sort.insertion_sort(nums))
     print(sort.quick_sort(nums))
+    print(sort.merge_sort(nums))
