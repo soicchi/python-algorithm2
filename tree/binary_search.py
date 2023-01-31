adb4c57cebd6fea1f1a6be3ece9fb45fbcba4dde
soichiro-nishizono <soicchi@icloud.com>
2023-01-31 21:48:02 +0900

    feat: created insert, inorder and search function in binary search tree

--- tree/binary_search.py
+++ tree/binary_search.py
@@ -17,6 +17,31 @@ def insert(node: Node, value: int) -> Node:
     return node
 
 
+def inorder(node: Node) -> None:
+    # Inorder: left, root, right
+    # preorder: root, left, right
+    # Postorder: left, right, root
+    if node is None:
+        return
+
+    inorder(node.left)
+    print(node.value)
+    inorder(node.right)
+
+
+def search(node: Node, value: int) -> bool:
+    if node is None:
+        return False
+
+    if node.value == value:
+        return True
+
+    if node.value > value:
+        return search(node.left, value)
+
+    return search(node.right, value)
+
+
 if __name__ == "__main__":
     root = None
     root = insert(root, 3)
@@ -26,4 +51,5 @@ if __name__ == "__main__":
     root = insert(root, 1)
     root = insert(root, 10)
     root = insert(root, 2)
-
+    inorder(root)
+    print(search(root, 8))
